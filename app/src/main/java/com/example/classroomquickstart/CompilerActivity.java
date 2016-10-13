f418c3ef3a72a6a168eef43dca6b4bb6c4ab9794
AMFIRNAS <aleem.firnas22@gmail.com>
2016-10-13 20:57:08 +0530

    Add existing file

---
+++ app/src/main/java/com/example/classroomquickstart/CompilerActivity.java
@@ -0,0 +1,54 @@
+package com.example.classroomquickstart;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.io.File;
+
+//import com.google.android.gms.common.api.GoogleApiClient;
+
+/**
+ * Created by Aleem Firnas on 10/9/2016.
+ */
+
+public class CompilerActivity extends Activity{
+    private static final int REQUEST_PICK_FILE = 1;
+
+    private TextView filePath;
+    private Button Browse;
+    private File selectedFile;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+
+    private GoogleApiClient client;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compiler);
+
+        filePath = (TextView)findViewById(R.id.file_path);
+        Browse = (Button) findViewById(R.id.browse);
+//        Browse.setOnClickListener(this);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
+
+        System.out.println("Hello World"):
+    }
+
+
+
+
+
+
+
+}
